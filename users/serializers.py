cc2e6b4346b266ae1023970fd3631dd95fb205a5
Farkhad Tutkabaev <farkhadnavoi@yandex.com>
2024-07-30 20:09:23 +0500

    api login and signup

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Profile
+from django.contrib.auth.models import User
 
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Profile
-        fields = ['user', 'bio', 'profile_picture']
+        fields = ['id', 'user', 'bio', 'profile_picture']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email']
